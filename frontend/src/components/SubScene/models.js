56b89e762e14ef98ade566595dc11c50b42a804c
dudewithoneleg <galvancromeo@gmail.com>
2023-10-26 04:51:19 -0500

    added camera pan and new positions

---
+++ frontend/src/components/SubScene/models.js
@@ -0,0 +1,39 @@
+const models = [
+    {
+        src: '../../../models/sofa-set.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0],
+        name: 'Sofas'
+    },
+    {
+        src: '../../../models/table.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0],
+        name: 'Tables'
+    },
+    {
+        src: '../../../models/endtables.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0],
+        name: 'End Tables'
+    },
+    {
+        src: '../../../models/bowl.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0]
+    },
+    {
+        src: '../../../models/lamp.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0],
+        name: 'Lamps'
+    },
+    {
+        src: '../../../models/carpet.gltf',
+        position: [0, 0, 0],
+        rotation: [0, 76.92, 0],
+        name: 'Carpets'
+    },
+]
+
+export default models
