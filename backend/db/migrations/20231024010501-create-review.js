e3da726efb57f2168864b4ab53cc7b4000588a43
dudewithoneleg <galvancromeo@gmail.com>
2023-11-05 20:37:41 -0600

    adjust migrations

--- backend/db/migrations/20231024010501-create-review.js
+++ backend/db/migrations/20231024010501-create-review.js
@@ -33,7 +33,7 @@ module.exports = {
         onDelete: "CASCADE"
       },
       review: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(2000),
         allowNull: false
       },
       rating: {
